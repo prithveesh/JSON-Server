68a740ae33f684b8ff90bbc6b2b7421181c52470
Prithveesh Goel <pgoe12@sapient.com>
2016-11-22 00:59:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var jsonServer = require('json-server')
+
+const fs = require('fs');
+const path = require('path');
+var jsonfolder = "./mocks/";
+var db = {};
+var files = fs.readdirSync(jsonfolder);
+files.forEach(function(file) {
+    if (path.extname(jsonfolder + file) === '.json') {
+        db[path.basename(jsonfolder + file, '.json')] = require("./" + path.join(jsonfolder, file));
+    }
+});
+
+// Returns an Express server
+var server = jsonServer.create();
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(jsonServer.defaults());
+// Returns an Express router
+var router = jsonServer.router(db);
+server.use(router);
+server.listen(3000);
